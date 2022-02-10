253214b0e25a0751f17a8aa0ff4f65e65c82dc41
Thomas Bauer <thomasb_dev@outlook.com>
2022-02-10 15:11:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_RPG_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
