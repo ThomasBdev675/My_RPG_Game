175fda2bd98cc384a2ab2f753ee86d0a132953de
Thomas Bauer <thomasb_dev@outlook.com>
2022-02-10 18:31:01 +0100

    add: initiall Character class design

---
+++ CharacterClasses.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_RPG_Game
+{
+    public class AvailableClasses
+    {
+        ///Define Mage class and their attributes
+        public class Mage
+        {
+            protected double strenght = 1;
+            protected double dexterity = 1;
+            protected double intelligence = 8;
+
+            ///Show stats
+            public void ShowStats()
+            {   ///Show base stats             
+                Console.WriteLine("Strenght: " + strenght);
+                Console.WriteLine("Dexterity: " + dexterity);
+                Console.WriteLine("Intelligence: " + intelligence);
+                ///Run stat calculation
+                CalculateCurrentStats();  
+            }
+
+            ///Calculate current stats  NOT YET DONE
+            protected void CalculateCurrentStats()
+            {
+                strenght += 1;
+                dexterity += 1;
+                intelligence += 1;
+                Console.WriteLine("Calculated Strenght: " + strenght );
+                Console.WriteLine("Calculated Dexterity: " + dexterity );
+                Console.WriteLine("Calculated Intelligence: " + intelligence );
+
+            }
+        }
+
+        public class Ranger
+        {
+            protected double strenght = 1;
+            protected double dexterity = 7;
+            protected double intelligence = 1;
+
+            ///Show stats
+            public void ShowStats()
+            {   ///Show base stats             
+                Console.WriteLine("Strenght: " + strenght);
+                Console.WriteLine("Dexterity: " + dexterity);
+                Console.WriteLine("Intelligence: " + intelligence);
+                ///Run stat calculation
+                CalculateCurrentStats();
+            }
+
+            ///Calculate current stats  NOT YET DONE
+            protected void CalculateCurrentStats()
+            {
+                strenght += 1;
+                dexterity += 1;
+                intelligence += 1;
+                Console.WriteLine("Calculated Strenght: " + strenght);
+                Console.WriteLine("Calculated Dexterity: " + dexterity);
+                Console.WriteLine("Calculated Intelligence: " + intelligence);
+
+            }
+        }
+
+        public class Rogue
+        {
+            protected double strenght = 2;
+            protected double dexterity = 6;
+            protected double intelligence = 1;
+
+            ///Show stats
+            public void ShowStats()
+            {   ///Show base stats             
+                Console.WriteLine("Strenght: " + strenght);
+                Console.WriteLine("Dexterity: " + dexterity);
+                Console.WriteLine("Intelligence: " + intelligence);
+                ///Run stat calculation
+                CalculateCurrentStats();
+            }
+
+            ///Calculate current stats  NOT YET DONE
+            protected void CalculateCurrentStats()
+            {
+                strenght += 1;
+                dexterity += 1;
+                intelligence += 1;
+                Console.WriteLine("Calculated Strenght: " + strenght);
+                Console.WriteLine("Calculated Dexterity: " + dexterity);
+                Console.WriteLine("Calculated Intelligence: " + intelligence);
+
+            }
+        }
+
+        public class Warrior
+        {
+            protected double strenght = 5;
+            protected double dexterity = 2;
+            protected double intelligence = 1;
+            ///Show stats
+            public void ShowStats()
+            {   ///Show base stats             
+                Console.WriteLine("Strenght: " + strenght);
+                Console.WriteLine("Dexterity: " + dexterity);
+                Console.WriteLine("Intelligence: " + intelligence);
+                ///Run stat calculation
+                CalculateCurrentStats();
+            }
+
+            ///Calculate current stats  NOT YET DONE
+            protected void CalculateCurrentStats()
+            {
+                strenght += 1;
+                dexterity += 1;
+                intelligence += 1;
+                Console.WriteLine("Calculated Strenght: " + strenght);
+                Console.WriteLine("Calculated Dexterity: " + dexterity);
+                Console.WriteLine("Calculated Intelligence: " + intelligence);
+
+            }
+        }
+    }
+}
+
+
+
--- Program.cs
+++ Program.cs
@@ -4,9 +4,57 @@ namespace My_RPG_Game
 {
     class Program
     {
+        ///Character Selector
+        ///Available values are: Mage, Ranger, Rogue, Warrior
+        static void CharacterSelector(string myCharacterSelection)            
+            {
+                try
+                {
+                    Console.WriteLine("Character selected: " + myCharacterSelection);
+                    switch (myCharacterSelection)
+                {
+                    case "Mage":
+                        AvailableClasses.Mage MyMage = new AvailableClasses.Mage();
+                        MyMage.ShowStats();
+                        break;
+                    case "Ranger":
+                        AvailableClasses.Ranger MyRanger = new AvailableClasses.Ranger();
+                        MyRanger.ShowStats();
+                        break;
+                    case "Rogue":
+                        AvailableClasses.Rogue MyRogue = new AvailableClasses.Rogue();
+                        MyRogue.ShowStats();
+                        break;
+                    case "Warrior":
+                        AvailableClasses.Warrior MyWarrior = new AvailableClasses.Warrior();
+                        MyWarrior.ShowStats();
+                        break;
+                    default:
+                        Console.WriteLine("You did not selected any available Character Type");
+                        break;
+                }
+            }
+                catch (Exception ex)
+                {
+                    //Exception NOT YET DONE
+                     Console.WriteLine(ex.Message);
+                }
+            finally
+            {
+                Console.WriteLine("Character Selection done");
+            }
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to our RPG console game");
+            CharacterSelector("test");
+
         }
     }
+    
+    
 }
